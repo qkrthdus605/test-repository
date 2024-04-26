cff16334ed77817caa89feb220adcfb29242fe5d
qkrthdus605 <qkrthdus605@gmail.com>
2024-04-26 16:41:14 +0900

    week3 day5

---
+++ week3/js-demo/function-demo.js
@@ -0,0 +1,3 @@
+function add1(x, y) {
+  return x + y;
+}
---
+++ week3/js-demo/object-demo.js
@@ -0,0 +1,13 @@
+let book = {
+  title: "책이름",
+  price: 20000,
+  description: "책 소개",
+};
+
+function print(obj) {
+  console.log(obj.title);
+  console.log(obj.price);
+  console.log(obj.description);
+}
+
+print(book);
